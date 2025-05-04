c316a4a8e6a3a1e1e494612a540e9c93b8c5a972
zedddie16 <zedddiezxc@gmail.com>
2025-05-04 20:11:47 +0200

    ub examples

--- examples/ub.rs
+++ examples/ub.rs
@@ -17,4 +17,8 @@ fn f1(a: &Cell<i32>, b: &Cell<i32>) {
     }
 } // Cell on the other hand, allows interior mutability by copying and pasting changing value
 // but can be used in one thread only.
-fn main() {}
+fn main() {
+    let a = [123, 456, 789];
+    let b = unsafe { a.get_unchecked(4) };
+    println!("{b}");
+}
