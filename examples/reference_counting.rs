b023b19f2f7489922f9d46f2dfcc1429b99cb24b
zedddie16 <zedddiezxc@gmail.com>
2025-05-04 18:18:05 +0200

    Reference counters example

--- examples/reference_counting.rs
+++ examples/reference_counting.rs
@@ -1,8 +1,11 @@
 use std::rc::Rc;
 
 fn main() {
+    let a1 = [1, 2, 3];
+    let b1 = a1.clone();
     let a = Rc::new([1, 2, 3]);
     let b = a.clone();
 
     assert_eq!(a.as_ptr(), b.as_ptr()); // same
+    assert_ne!(a1.as_ptr(), b1.as_ptr()); // not
 }
