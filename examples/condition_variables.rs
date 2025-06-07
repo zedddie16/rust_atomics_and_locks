ffb7aced08ce65caafc51d80cd4a0d109205a4c6
zedddie16 <zedddiezxc@gmail.com>
2025-06-07 14:46:45 +0200

    feat: some explanation for me

--- examples/condition_variables.rs
+++ examples/condition_variables.rs
@@ -25,7 +25,13 @@ fn main() {
         });
 
         for i in 0.. {
+            // Mutex is locked, pushed and immedeately
+            // released
             queue.lock().unwrap().push_back(i);
+            // if there are more than one thread waiting
+            // on the same condition variable, and notify_one
+            // is called, then only one(random, choosed by OS)
+            // thread will awake
             not_empty.notify_one();
             thread::sleep(Duration::from_secs(1));
         }
